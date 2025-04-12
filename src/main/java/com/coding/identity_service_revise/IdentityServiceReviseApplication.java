494c221f67eacc3c1969b40644cd4f5a55f6bcb9
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2025-04-12 22:13:20 +0700

    initial project

---
+++ src/main/java/com/coding/identity_service_revise/IdentityServiceReviseApplication.java
@@ -0,0 +1,13 @@
+package com.coding.identity_service_revise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServiceReviseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityServiceReviseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/identity_service_revise/IdentityServiceReviseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.identity_service_revise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceReviseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
